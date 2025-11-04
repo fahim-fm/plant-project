dccddf9f62a01357cd104be9802238f12df798d3
Fahim <fahimmuntasir234@gmail.com>
2025-11-04 18:54:50 +0600

    feat: implement scroll-up button functionality and active link highlighting; enhance footer layout and styling

--- main.js
+++ main.js
@@ -16,9 +16,34 @@ navLink.forEach(n => n.addEventListener('click', () => {
 
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById('scroll-up')
+    if (window.scrollY > 250) {
+        scrollUpBtn.classList.add('bottom-4')
+        scrollUpBtn.classList.remove('-bottom-1/2')
+    } else {
+           scrollUpBtn.classList.add('-bottom-1/2')
+        scrollUpBtn.classList.remove('bottom-4')
+    }
+}
+
+window.addEventListener('scroll', scrollUp)
 
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+    const header = document.getElementById('navbar')
+    if (window.scrollY > 250) {
+        header.classList.add('border-b', 'border-yellow-500')
+       
+    } else {
+           
+        header.classList.remove('border-b', 'border-yellow-500')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
+
 
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
@@ -55,6 +80,39 @@ autoplay:{
 
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
-
+const activeLink = () => {
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.nav-link')
+    let current = 'home'
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if(this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute('id')
+        }      
+})
+navLinks.forEach(item => {
+    item.classList.remove('active')
+    
+        if(item.href.includes(current)) {
+            item.classList.add('active')
+        }
+    })
+}    
+window.addEventListener('scroll',activeLink)
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+const sr=new ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:300,
+    reset:true
+})
+sr.reveal('.home__data,.about__top,.popular__top,.review__top,.review__swiper, .footer__icon,.footer__content, .copy__right')
+sr.reveal('.home__image',{delay: 500,scale: 0.5})
+
+sr.reveal('.service__card ,.popular__card',{interval:100})
+sr.reveal('.about__leaf',{delay:1000,origin:'right'})
+sr.reveal('.about__item__1-content, .about__item__2-img',{origin:'right'})
+sr.reveal('.about__item__2-content, .about__item__1-img',{origin:'left'})
+sr.reveal('.review__leaf, .footer__floral',{delay:1000, origin:'left'})
